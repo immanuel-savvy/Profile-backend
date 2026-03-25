1e5d9cd20064fcf7ecd1a020146272e3fe7b03d2
immanuel-savvy <immanuelsavvy@gmail.com>
2026-03-25 12:53:11 +0100

    m

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ let server = http.createServer(handler);
 let port = process.env.PORT || 4000;
 
 server.listen(port, async () => {
+  // console.log(
+  //   await (
+  //     await PROFILES()
+  //   ).deleteMany({
+  //     profile: "bbc10363-bcf6-409e-93d6-eef256c0c92c",
+  //     phone: "+2349074991739",
+  //   }),
+  // );
   console.log(`Profile API is listening on http://localhost:${port}`);
 });
