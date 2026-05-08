ff633d3335f57ddd2c74d2b1e8c0944803c8b4b0
immanuel-savvy <immanuelsavvy@gmail.com>
2026-05-08 13:14:19 +0100

    cmm

--- ds/conn.js
+++ ds/conn.js
@@ -6,7 +6,7 @@ const conn = () => {
 
   let db = new Mongo({
     db_url: process.env.MONGODB_URI,
-    db_name: process.env.PLATFORM_URI,
+    db_name: process.env.PLATFORM_URI.replace(/\./g, "-"),
   });
 
   db_instance = db;
